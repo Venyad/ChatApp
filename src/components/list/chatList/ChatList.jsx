e0f42ed3869e6dd75bde5358dc60fc362fe44bc9
Venya <venyadurgam25@gmail.com>
2024-08-26 19:06:25 -0500

    Updating the chat List component

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -4,7 +4,13 @@ import "./chatList.css"
 const ChatList = () => {
   return (
     <div className='chatList'>
-      ChatList
+      <div className='search'>
+        <div className='searchBar'>
+            <img src="./search.png" alt="" />
+            <input type="text" placeholder='Search' />
+        </div>
+        <img src="./plus.png" alt="" className='add'/>
+      </div>
     </div>
   )
 }
