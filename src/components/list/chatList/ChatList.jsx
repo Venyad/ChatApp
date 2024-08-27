970eadcf721433cfc2b17d472dda18d2eb9f7cac
Venya <venyadurgam25@gmail.com>
2024-08-26 19:47:04 -0500

    Updated Chat component

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useState } from 'react'
 import "./chatList.css"
 
 const ChatList = () => {
+    const [addMode, setAddmode] = useState(false)
   return (
     <div className='chatList'>
       <div className='search'>
@@ -9,7 +11,32 @@ const ChatList = () => {
             <img src="./search.png" alt="" />
             <input type="text" placeholder='Search' />
         </div>
-        <img src="./plus.png" alt="" className='add'/>
+        <img src={addMode ? "./minus.png" : "./plus.png"} alt="" className='add'
+        onClick={()=> setAddmode((prev) => !prev)}/>
+      </div>
+      <div className='item'>
+        <img src="./avatar.png" alt="" />
+        <div className='texts'>
+            <span>Jane Doe</span>
+            <p>Hello</p>
+        </div>
+
+      </div>
+      <div className='item'>
+        <img src="./avatar.png" alt="" />
+        <div className='texts'>
+            <span>Jane Doe</span>
+            <p>Hello</p>
+        </div>
+
+      </div>
+      <div className='item'>
+        <img src="./avatar.png" alt="" />
+        <div className='texts'>
+            <span>Jane Doe</span>
+            <p>Hello</p>
+        </div>
+
       </div>
     </div>
   )
