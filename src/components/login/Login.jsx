45326ef1da9c9aae6f1b1c4d45cce91ada5e3439
Venya <venyadurgam25@gmail.com>
2024-08-30 16:32:36 -0500

    Updated Login functionality

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -52,8 +52,23 @@ const Login = () => {
         }
     }
 
-    const handleLogin = e => {
+    const handleLogin = async(e) => {
         e.preventDefault()
+        setLoading(true);
+
+        const formData = new FormData(e.target);
+        const {email, password } = Object.fromEntries(formData);
+
+        try{
+            await signInWithEmailAndPassword(auth, email, password);
+
+        }catch(err){
+            console.log(err);
+            toast.error(err.message);
+
+        } finally{
+            setLoading(false);
+        }
     }
 
 
