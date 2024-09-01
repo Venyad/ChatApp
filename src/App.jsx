aaea61b26e24cd213d21cb605f2b236c39da044d
Venya <venyadurgam25@gmail.com>
2024-08-31 22:40:07 -0500

    created userStore component

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,26 @@ import List from "./components/list/List"
 import Login from "./components/login/Login"
 import Notification from "./components/notification/Notification"
 import { auth } from "./lib/firebase"
+import { useUserStore } from "./lib/userStore"
 import "./index.css"
 
 const App = () => {
 
-  const user = false;
+  const {currentUser, isLoading, fetchUserInfo} = useUserStore()
   useEffect(() => {
     const unSub = onAuthStateChanged(auth,(user)=>{
-      console.log(user)
+      fetchUserInfo(user.uid);
     })
     return () => {
       unSub();
     }
-  },[])
+  },[fetchUserInfo]);
+  console.log(currentUser)
+  if (isLoading) return <div className="loading">Loading...</div>
   return (
     <div className='container'>
       {
-        user ? (
+        currentUser ? (
           <>
             <List />
             <Chat />
