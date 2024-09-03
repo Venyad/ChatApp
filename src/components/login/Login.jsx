23b7bfc1af688eed0f40ea14f7066965420acd65
Venya <venyadurgam25@gmail.com>
2024-09-03 01:38:01 -0500

    Updated Login Page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -5,6 +5,7 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "fire
 import { auth, db } from "../../lib/firebase";
 import { doc, setDoc } from "firebase/firestore"
 import upload from "../../lib/upload";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 const Login = () => {
     const [avatar, setAvatar] = useState({
